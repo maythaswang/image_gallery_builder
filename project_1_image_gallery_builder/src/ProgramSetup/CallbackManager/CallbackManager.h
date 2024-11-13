6c786ed6c5487d7a472f0299d9d4e2c3932ce085
Maythas <maythas.wangcharoenwong@gmail.com>
2024-11-14 02:24:05 +0700

    feat: Added Skeleton for CallbackManager.h

--- project_1_image_gallery_builder/src/ProgramSetup/CallbackManager/CallbackManager.h
+++ project_1_image_gallery_builder/src/ProgramSetup/CallbackManager/CallbackManager.h
@@ -1,1 +1,48 @@
-// Manages all callbacks
+#ifndef CALLBACK_MANAGER_H
+#define CALLBACK_MANAGER_H
+#pragma once
+#include <GLCommon.h>
+class CallbackManager
+{
+public:
+    /**
+     * @brief Construct a new Callback Manager
+     *
+     * @param window glfw window where we want to use as our display window
+     */
+    CallbackManager(GLFWwindow *);
+
+    /**
+     * @brief Main function for processing input
+     *
+     */
+    void process_input();
+
+private:
+    GLFWwindow *window;
+
+    /**
+     * @brief Handle each mouse and keyboard input accordingly.
+     *
+     */
+    void handle_input();
+
+    /**
+     * @brief Set the window and camera aspect ratio when framebuffer gets resized.
+     *
+     */
+    void set_window_resize_callback();
+
+    /**
+     * @brief Set the cursor position callback.
+     *
+     */
+    void set_cursor_position_callback();
+
+    /**
+     * @brief Set the keyboard callback.
+     *
+     */
+    void set_keyboard_callback();
+};
+#endif
