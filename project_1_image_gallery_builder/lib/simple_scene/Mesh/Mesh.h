0cdf097a26bc97b3de984f824ad3d19bfaf7aff1
Maythas <maythas.wangcharoenwong@gmail.com>
2024-11-17 01:34:53 +0700

    feat: Created a struct for mesh.

--- project_1_image_gallery_builder/lib/simple_scene/Mesh/Mesh.h
+++ project_1_image_gallery_builder/lib/simple_scene/Mesh/Mesh.h
@@ -1,14 +1,19 @@
 #ifndef SS_MESH_H
 #define SS_MESH_H
 #pragma once
-// Mesh class that stores vertices, indices, texture_id, textures <will be use for generating a scene later when batching.> 
+#include <lin_alg/lin_alg.h>
+#include <vector>
 
+// TODO: Will be used for generating a scene later when batching.
+// This struct will use 1 Mesh 1 material method for simplicity before batching
 namespace ss
 {
-    class Mesh
+    struct Mesh
     {
-        public:
-        private:
+        std::vector<lin_alg::vec3> vertices;
+        std::vector<lin_alg::vec3> indices;
+        std::vector<float[2]> tex_coord;
+        float mat_id;
     };
 }
 
